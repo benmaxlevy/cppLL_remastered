d28c30d71c57da55cc2b0bdcce0457a778bc5335
Ben Levy <general@simplexwebsites.com>
2020-10-10 16:51:31 -0400

    Refine add method under linked list (class) (take in value and pointer to next node). Finish find, findValue, and addAfter. Next, do remove.

--- cppLL_remastered/Source.cpp
+++ cppLL_remastered/Source.cpp
@@ -56,68 +56,118 @@ public:
 	}
 
 	//make a node (returns pointer to a node)
-	static Node* createNode() {
-		return new Node;
+	static Node* createNode(const int value = 0, Node* next = nullptr) {
+		return new Node(value, next);
 	}
 
 	//create a new node
-	int add(int value) {
+	int add(const int value) {
 
 		//if the pointer to the head node is null, create it!
 		if (this->head == nullptr) {
-			this->head = createNode();
-			this->head->value = value;
-			this->head->next = nullptr;
+			this->head = createNode(value);
 
 			return 0;
 		}
 		//if it's not, wait until we find the end of the ll, and then create the pointer to the next node.
 		else {
 			//make a reference point
-			Node* currentNode = this->head;
+			auto* current_node = this->head;
 
-			while (currentNode->next != nullptr) {
-				currentNode = currentNode->next;
+			while (current_node->next != nullptr) {
+				current_node = current_node->next;
 			}
-			currentNode->next = createNode();
-			currentNode->next->value = value;
-			currentNode->next->next = nullptr;
+			current_node->next = createNode();
+			current_node->next->value = value;
+			current_node->next->next = nullptr;
 
 			return 0;
 		}
 	}
 
-	//find value of node at specified index
-	int find(int index) {
-		
-		//make a reference point
-		Node* currentNode = this->head;
+	//find node, and return a pointer to that node
+	Node* find(const int index) {
+		auto* current_node = this->head;
 
-		for (int i = 0; i < index; i++) {
-			//check if the pointer to the next node is nullptr
-			if (currentNode->next == nullptr) {
-				//invalid! (cannot reach the index specified).
-				return 1;
+		for (auto i = 0; i < index; i++)
+		{
+			if (current_node->next == nullptr)
+			{
+				//check if the pointer to the next node is nullptr
+				return nullptr;
 			}
-			else {
-				currentNode = currentNode->next;
+			else
+			{
+				current_node = current_node->next;
 			}
 		}
 
-		return currentNode->value;
+		//return the node at the specified index
+		return current_node;
+
+	}
+	
+	//find value of node at specified index
+	int findValue(const int index) {
+		const auto* found_node = this->find(index);
+
+		//check if the found node is null or not
+		if(found_node == nullptr)
+		{
+			return 1; //error
+		} else
+		{
+			return found_node->value;	
+		}
+		
 	}
 
 	//add node after index
-	int addAfter(int index, int value) {
+	int addAfter(const int value, const int index)
+	{
+		//store the head node for context
+		auto* current_node = this->head;
 
+		for(int i = 0; i < index; i++)
+		{
+			if(current_node->next == nullptr)
+			{
+				return 1; //return error
+			}
+			else
+			{
+				current_node = current_node->next;
+			}
+		}
+
+		//check if the next node (of the node that we want to add after) is a nullptr
+		if(current_node->next == nullptr) 
+		{
+			current_node->next = createNode(value);
+			return 0;
+		} else //if the next node exists
+		{
+			//save the pointer to the next node, so that we can insert a node before, and "make it seamless"
+			auto* to_save = current_node->next;
+			
+			current_node->next = createNode(value, to_save);
+
+			return 0; //success! 
+		}
+	}
+
+	int remove(const int index)
+	{
+		
 	}
 };
 
 
 int main() {
-	auto ll = new LinkedList;
+	auto* ll = new LinkedList;
 	ll->add(5);
 	ll->add(6);
 	ll->add(50);
+	ll->addAfter(100, 1);
 
 }
